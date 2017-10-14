9a65a671dd2e389633ac228574fdcb8c9fc51dd8
gabriel <gabrielmesquita@cjr.org.br>
2017-10-14 16:26:17 -0300

    Removing rubocop offense.

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,11 +1,9 @@
 class Quiz < ApplicationRecord
-  ANSWER_OPTIONS = {
-                      answer1: %w[Yes No],
-                      answer2: %w[1\ month 6\ months 1\ year],
-                      answer3: %w[Java Ruby Javascript C],
-                      answer4: %w[less\ than\ 1\ year 1-2\ years 2-3\ years 3-4\ years],
-                      answer5: %w[No! Yes Maybe\ :(]
-                    }.freeze
+  ANSWER_OPTIONS = {answer1: %w[Yes No],
+                    answer2: %w[1\ month 6\ months 1\ year],
+                    answer3: %w[Java Ruby Javascript C],
+                    answer4: %w[less\ than\ 1\ year 1-2\ years 2-3\ years 3-4\ years],
+                    answer5: %w[No! Yes Maybe\ :(]}.freeze
 
   def self.retrieve_answers_distribution
     ANSWER_OPTIONS.keys.map {|key| build_answer_distribution(key) }
